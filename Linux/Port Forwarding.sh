865c3f5db7b0513ca6ed172313bb9f2f13631bcf
Frivolous-Sites <35827420+Frivolous-Sites@users.noreply.github.com>
2019-02-15 01:07:39 -0500

    Update Port Forwarding.sh

--- Linux/Port Forwarding.sh
+++ Linux/Port Forwarding.sh
@@ -11,4 +11,4 @@ PORT_OUT=[dest_port]
 echo "1" > /proc/sys/net/ipv4/ip_forward
 $IPTBL -A PREROUTING -t nat -i $IF_IN -p tcp --dport $PORT_IN -j DNAT --to-destination ${IP_OUT}:${PORT_OUT}
 $IPTBL -A FORWARD -p tcp -d $IP_OUT --dport $PORT_OUT -j ACCEPT
-$IPTBL -A POSTROUTING -t nat -j MASQUERADE
+#$IPTBL -A POSTROUTING -t nat -j MASQUERADE
